7be0cef9e5930fc0974fb9df68a0c9a96c14b7cb
MayloParra <maylo.parra@ucr.ac.cr>
2024-05-23 02:06:11 -0600

    nada

--- POO/Servidor.java
+++ POO/Servidor.java
@@ -8,6 +8,7 @@ public class Servidor {
   public static void main(String argv[]) throws Exception {
         String clientSentence;
         String capitalizedSentence;
+ 
         ServerSocket welcomeSocket = new ServerSocket(6789);
 
         while (true) {
@@ -17,7 +18,10 @@ public class Servidor {
 
             clientSentence = inFromClient.readLine();
             capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+           outToClient.writeBytes(capitalizedSentence);
+
         }
+
     }
+
 }//Fin de la clase Servidor
